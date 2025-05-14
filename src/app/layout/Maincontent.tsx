eae5a49330148f9f9971a3b75f571ff04eb1b54f
DirectoLEE <lyg5779@naver.com>
2025-05-14 19:55:13 +0900

    레이아웃 코드 수정

---
+++ src/app/layout/Maincontent.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Maincontent: FC = () => {
+    return (
+        <div>
+            <h1>Welcome to My Site</h1>
+        </div>
+    );
+}
+
+export default Maincontent;
---
+++ src/app/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Sidebar: FC = () => {
+    return (
+        <div>
+            <h1>Welcome to My Site</h1>
+        </div>
+    );
+}
+
+export default Sidebar;
